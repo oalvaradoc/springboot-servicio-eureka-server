41ddded31f22696eb094af7e46c19ca423a32739
Omar Alvarado <omar777g@gmail.com>
2022-11-21 00:35:36 -0500

    first commit

---
+++ src/test/java/com/spring/app/eureka/SpringbootServicioEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.app.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServicioEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
